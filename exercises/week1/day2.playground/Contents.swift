a74c3f6f64dd3f00757e14a9fa613a1e070e4fcc
yitzchaks <136307549+yitzchaks@users.noreply.github.com>
2023-06-13 10:28:28 +0300

    Replacing forced unwrapping with nil-coalescing operator, and changes in indentation

--- exercises/week1/day2.playground/Contents.swift
+++ exercises/week1/day2.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+
 //Slice Sizing 1
 func sliceSize(diameter: Double?, slices: Int?) -> Double?{
     switch (diameter, slices) {
@@ -10,14 +11,17 @@ func sliceSize(diameter: Double?, slices: Int?) -> Double?{
     }
 }
 
+//Tests
 sliceSize(diameter: 16, slices: 12)
 sliceSize(diameter: nil, slices: 8)
+
 
 //Slice Sizing 2
 func biggestSlice(diameterA: String, slicesA: String, diameterB: String, slicesB: String) -> String{
     let sizeA: Double? = sliceSize(diameter: Double(diameterA), slices: Int(slicesA))
     let sizeB: Double? = sliceSize(diameter: Double(diameterB), slices: Int(slicesB))
     var bigger: String?
+    
     switch (sizeA, sizeB) {
     case let (a?, b?):
         if a > b {
@@ -32,21 +36,25 @@ func biggestSlice(diameterA: String, slicesA: String, diameterB: String, slicesB
     default:
         return "Neither slice is bigger"
     }
-    return "Slice \(bigger!) is bigger"
     
+    return "Slice \(bigger ?? "") is bigger"
+    // Or return "Slice \(bigger!) is bigger"
 }
 
+//Tests
 biggestSlice(diameterA: "10", slicesA: "6", diameterB: "14", slicesB: "12")
 biggestSlice(diameterA: "10", slicesA: "6", diameterB: "12", slicesB: "8")
 biggestSlice(diameterA: "Pepperoni", slicesA: "6", diameterB: "hhh", slicesB: "12")
 
+
 //K times (exercise 3)
 func applyKTimes(_ K: Int, _ closure: () -> ()){
-    for _ in 0..<K {
+    for _ in 0 ..< K {
         closure()
     }
 }
 
+//Tests
 applyKTimes(3) {
     print("We Heart Swift")
 }
@@ -76,6 +84,7 @@ func finalLocation(startLocation: (x: Int, y: Int), steps: [Direction]) -> (Int,
     return location
 }
 
+//Tests
 var location = (x: 0, y: 0)
 var steps: [Direction] = [.up, .up, .left, .down, .left]
 
