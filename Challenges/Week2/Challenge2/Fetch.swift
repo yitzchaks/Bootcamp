0581eb095e79ffe8d042e7a3894dd6703f54a5c9
yitzchaks <136307549+yitzchaks@users.noreply.github.com>
2023-06-25 10:40:29 +0300

    Adding a fetch file (dropped before)

---
+++ Challenges/Week2/Challenge2/Fetch.swift
@@ -0,0 +1,53 @@
+//
+//  Fetch.swift
+//  Challenge2
+//
+//  Created by Yitzchak Schechter on 25/06/2023.
+//
+
+import UIKit
+
+extension URLSession {
+    
+    func fetch<T: Codable> (
+        _ url: String,
+        method: String = "GET",
+        headers: [String: String]? = nil,
+        body: [String: Any]? = nil,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) {
+        guard let url = URL(string: url) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        if let headers = headers {
+            for (key, value) in headers {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        if let body = body {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        }
+        
+        self.dataTask(with: request) {
+            (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            if let data = data {
+                do {
+                    if(T.self == String.self) {
+                        let object = String(data: data, encoding: .utf8)
+                        completion(.success(object as! T))
+                        return
+                    }
+                    let object = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(object))
+                } catch let decoderError {
+                    completion(.failure(decoderError))
+                }
+            }
+        }.resume()
+    }
+}
+
